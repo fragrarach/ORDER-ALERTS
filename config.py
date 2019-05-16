961670645071ff10b0dfc88baadedac7b6ffe8d2
Jan Zalobinski <39808183+fragrarach@users.noreply.github.com>
2019-05-16 10:44:30 -0400

    Removed cursor method from Config class

--- config.py
+++ config.py
@@ -7,6 +7,3 @@ class Config:
     def __init__(self):
         self.sigm_connection, self.sigm_db_cursor = sigm_connect(Config.LISTEN_CHANNEL)
         self.log_connection, self.log_db_cursor = log_connect()
-
-    def sigm_db_cursor(self):
-        return self.sigm_db_cursor
